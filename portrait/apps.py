55dcc91b0f49a71ac7473a4ccdeb117b4899f788
Daniel Pawlak <daniel.pawlak.und@gmail.com>
2020-11-03 10:07:15 -0800

    added signal to generate default profile pic when user is created

--- portrait/apps.py
+++ portrait/apps.py
@@ -4,5 +4,3 @@ from django.apps import AppConfig
 class PortraitConfig(AppConfig):
     name = 'portrait'
     
-    def ready(self):    
-        import portrait.signals
--- portrait/models.py
+++ portrait/models.py
@@ -1,9 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
 
 class Portrait(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     def __str__(self):
-        return f'{self.user.username} Profile'
+        return str(self.user)
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Portrait.objects.create(user=instance)
+        print("Profile created!")
+
+post_save.connect(create_profile, sender=User)
+
+def update_profile(sender, instance, created, **kwargs):
+    if created == False:
+        instance.portrait.save()
+        print("Profile updated!")
+  
+post_save.connect(update_profile, sender=User)
